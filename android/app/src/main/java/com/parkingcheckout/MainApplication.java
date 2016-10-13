ae011cd3ec87b3c47fe9723ef3df6f65afeecccf
David <ddsun@ucsc.edu>
2016-10-13 15:41:00 -0700

    Successfully updated AndroidManifest (step 5)

--- android/app/src/main/java/com/parkingcheckout/MainApplication.java
+++ android/app/src/main/java/com/parkingcheckout/MainApplication.java
@@ -3,35 +3,44 @@ package com.parkingcheckout;
 import android.app.Application;
 import android.util.Log;
 
+import com.facebook.appevents.AppEventsLogger;
 import com.facebook.react.ReactApplication;
 import com.magus.fblogin.FacebookLoginPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.facebook.FacebookSdk;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        protected boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            return Arrays.<ReactPackage>asList(
+                new MainReactPackage(),
+                new FacebookLoginPackage()
+            );
+        }
+    };
+
     @Override
-    protected boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
     }
 
     @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new FacebookLoginPackage()
-      );
+    public void onCreate() {
+        super.onCreate();
+        FacebookSdk.sdkInitialize (getApplicationContext());
+        com.facebook.appevents.AppEventsLogger.activateApp (this);
     }
-  };
-
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-      return mReactNativeHost;
-  }
 }
