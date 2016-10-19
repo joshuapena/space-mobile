2e258fad1c80641e58e0276473e6097c21d2d85b
Tyler <tylertagawa@gmail.com>
2016-10-19 14:35:12 -0700

    current user is now currect.

--- Signup.js
+++ Signup.js
@@ -4,64 +4,75 @@ import Button from 'react-native-button';
 
 
 var firebase = require ('firebase');
-/*
-var config = {
+/*var config = {
     apiKey: "AIzaSyAd7nS1dCGQILFxxZ5Jwsla5wy1rnbEI_M",
     authDomain: "space-252ee.firebaseapp.com",
     databaseURL: "https://space-252ee.firebaseio.com",
     storageBucket: "gs://space-252ee.appspot.com",
 }
-firebase.initializeApp (config);
+firebase.initializeApp (config);*/
 
-var db = firebase.database();
-var users = db.ref("users");
-*/
+//var db = firebase.database();
+//var users = db.ref("users");
 export default class Signup extends Component {
     constructor (props) {
         super (props);
         this.state = {text : 'this text will be updated by typing'};
     }
 
+
     buttonpress() {
         console.log ('button has been pressed');
         let email = this.state.email;
         let password = this.state.password;
-        var error_ = false;
         var user = firebase.auth().createUserWithEmailAndPassword (email, password).catch (function (error) {
-            if (error.code) {
-                error_ = true;
-                switch (error.code) {
-                    case "auth/email-already-in-use": 
-                        console.log ('email already in use');
-                        alert ('kill yourself');
-                        break;
+            alert('button has error');
+            switch (error.code) {
+                case "auth/email-already-in-use": 
+                    console.log ('email already in use');
+                    alert ('kill yourself');
+                    break;
 
-                    case "auth/invalid-email":
-                        alert ('please enter a valid email');
-                        break;
-                    
-                    case "auth/operation-not-allowed":
-                        alert ('your account has been disabled');
-                        break;
+                case "auth/invalid-email":
+                    alert ('please enter a valid email');
+                    break;
+                
+                case "auth/operation-not-allowed":
+                    alert ('your account has been disabled');
+                    break;
 
-                    case "auth/weak-password":
-                        alert ('please enter a stronger password');
-                        break;
+                case "auth/weak-password":
+                    alert ('please enter a stronger password');
+                    break;
 
-                    default: alert ('error creating account :/');
-                }
-            }
-        });
-        if (!error_) {
+                default: 
+                    alert ('error creating account :/');
+                    alert(error)
+            }   
+        }).then(function(resp) {
             alert ('account successfully created');
-            console.log (user);
-        }
+            var curr = firebase.auth().currentUser;
+            alert("user email"+curr.email);
+        });
     }
 
    /* userInfoToDb() {
         var currentUser = firebase.auth().currentUser;
-        var uidRef = db.child("users").set(currentUser.uid);
+       // var uidRef = db.child("users").set(currentUser.uid);
     } */
+    postUserData(myJson) {
+        return fetch('https://space-ucsc.herokuapp.com/createUser', myJson)
+        .then((response) => response.json())
+        .then((responseJson) => {
+          console.log("id1: ", responseJson.testid1);
+          console.log("id2: ", responseJson.testid2);
+          console.log("code: ", responseJson.code)
+          return responseJson.code;
+        })
+        .catch((error) => {
+        console.error(error);
+        });
+ }
 
     logUserInfoOnPress() {
         var currentUser = firebase.auth().currentUser;
