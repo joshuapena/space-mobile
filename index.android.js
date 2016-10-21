32032b21f2130ac3b97ff7884f29e6915a5faf00
David <ddsun@ucsc.edu>
2016-10-21 13:44:41 -0700

    added firebase initialization to index.android.js

--- index.android.js
+++ index.android.js
@@ -10,6 +10,14 @@ import MyListView from './MyListView';
 import SpaceListView from './SpaceListView';
 import MyModal from './MyModal';
 
+var firebase = require ('firebase');
+var config = {
+    apiKey: "AIzaSyAd7nS1dCGQILFxxZ5Jwsla5wy1rnbEI_M",
+    authDomain: "space-252ee.firebaseapp.com",
+    databaseURL: "https://space-252ee.firebaseio.com",
+    storageBucket: "gs://space-252ee.appspot.com",
+}
+firebase.initializeApp (config);
 
 class ParkingCheckout extends Component {
 
