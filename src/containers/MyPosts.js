ee86cbf448a1c6a85e8709afb9e996604cd167c2
Matthew Kim <madkim@ucsc.edu>
2016-10-24 20:01:59 -0700

    Added back button to MyPosts page, also put postings in a listview using native-base. Need to get postings to load quicker.

--- src/containers/MyPosts.js
+++ src/containers/MyPosts.js
@@ -1,23 +1,11 @@
 import React, {Component} from 'react';
 import {Navigator, ListView, StyleSheet, Text, TextInput, View, Image} from 'react-native';
+import { Container, Content, Thumbnail, Button, Icon, Header, Title, List, ListItem, Footer, FooterTab } from 'native-base';
 
-import Button from 'react-native-button' ;
 const Dimensions = require('Dimensions');
-
 const TimerMixin =  require('react-timer-mixin');
 
 var firebase = require ('firebase');
-
-import Icon from 'react-native-vector-icons/FontAwesome';
-const myIcon = (<Icon name="rocket" size={30} color="#900" />);
-const xIcon = (<Icon name="times" size={30} color="#100" />);
-const windowDims = Dimensions.get('window');
-
-const customTextButton = (
-  <Icon.Button name="facebook" backgroundColor="#3b5998">
-    <Text style={{fontFamily: 'Arial', fontSize: 15}}>Login with Facebook</Text>
-  </Icon.Button>
-);
 
 export default class MyPosts extends Component {
 
@@ -74,32 +62,12 @@ export default class MyPosts extends Component {
           });
         });
       });
-      //console.log("this is array of data " + this.state.arrOfData);
-      // this.setState({
-      //   dataSource : this.state.dataSource.cloneWithRows(this.state.arrOfData)
-      // });
-      //console.log("dataSource was updated");
     }
-    //  console.log("myArrWithData " + myStateArr.toString());
     console.log("myArrWithData " + JSON.stringify(myStateObj));
     this.setState({
-      dataSource : this.state.dataSource.cloneWithRows(myStateObj)
+      dataSource : myStateObj
     })
   }
-
-  //  fetch('https://space-ucsc.herokuapp.com/viewList')
-  //    .then((response) => response.json())
-  //    .then((responseJson) => {
-  //      console.log("GET /test : ", responseJson.code);
-  //      this.setState({
-  //        dataSource : this.state.dataSource.cloneWithRows(responseJson.spaceListing)
-  //      });
-   //
-  //      responseJson.code;
-  //    })
-  //    .catch((error) => {
-  //      console.error(error);
-  //    });
 
 
   componentDidMount(){
@@ -108,22 +76,32 @@ export default class MyPosts extends Component {
       this.getTestData();
     }, 3000)
   }
+
   componentWillUnmount() {
     clearInterval(this.timer);
   }
 
+
  render() {
    return (
-     <View >
-      <Text> List of Spaces</Text>
-       <ListView
-         enableEmptySections={true} // this line mutes a warning message that applys to
-         //cloneWithRowsAndSections, however, we use cloneWithRows so it is irrelevant to us
-         dataSource={this.state.dataSource}
-         renderRow={(rowData) => <Text> {xIcon}My price is {rowData.price}, for a {rowData.type}. {"\n"}It is at {rowData.address} </Text>}
-         />
-         <Button onPress={() => {this._navigateBack()}}> Cancel </Button>
-     </View>
+      <Container style={{backgroundColor: 'white'}}>
+      <Header style={{backgroundColor: '#e74c3c'}}>
+        <Button transparent onPress={() => this. _navigateBack()}>
+          <Icon name='ios-arrow-back' />
+        </Button>
+        <Title>My Posts</Title>
+      </Header>
+
+        <Content>
+          <List dataArray={this.state.dataSource}
+              renderRow={(item) => 
+                <ListItem>
+                <Text> My price is {item.price}, for a {item.type}. {"\n"}It is at {item.address} </Text>
+                </ListItem>
+                }>
+          </List>
+        </Content>
+    </Container>
    );
  }
 }
