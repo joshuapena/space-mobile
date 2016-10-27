f114089d2e43f6caab15d477e5c1bcf352041101
rlopezlu <rlopezlu@ucsc.edu>
2016-10-27 03:21:29 -0700

    use spinners while loading lists

--- src/containers/MyPosts.js
+++ src/containers/MyPosts.js
@@ -8,6 +8,9 @@ import React, {Component} from 'react';
 import {Navigator, ListView, StyleSheet, Text, TextInput, View, Image} from 'react-native';
 import { Container, Content, Thumbnail, Button, Icon, Header, Spinner, Title, List, ListItem, Footer, FooterTab } from 'native-base';
 
+var renderIf = require('render-if');
+
+
 const Dimensions = require('Dimensions');
 const TimerMixin =  require('react-timer-mixin');
 
@@ -20,14 +23,12 @@ export default class MyPosts extends Component {
 
       const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
       this.state = {
-        dataSource: ds.cloneWithRows([]),
-        arrOfData : [],
-        objOfData : {}
+        spinnerState: true,
+        dataSource: {},
       };
     }
 
   mixins: [TimerMixin]
-
   _navigateSignUp(){
     this.props.navigator.push({
       name: 'Hostspace', // Matches route.name
@@ -55,39 +56,38 @@ export default class MyPosts extends Component {
     var currentUser = firebase.auth().currentUser;
     var myState = this.state;
     var self = this;
-    var myStateArr = this.state.arrOfData;
-    var myStateObj = this.state.objOfData;
-    // var objWithData = {};
-    // var myArrWithData = [];
-    //alert(currentUser.email);
+    var myStateObj = this.state.dataSource;
     if (currentUser) {
       firebase.database().ref('/users/' + currentUser.uid + "/listing/").once('value').then(function(snapshot) {
-        //console.log(snapshot);
         snapshot.forEach(function(childSnapshot){
           console.log("the id of posts is: "+ childSnapshot.key);
           firebase.database().ref('/listings/' + childSnapshot.key).once('value').then(function(postSnapshot){
             var postKey = postSnapshot.key;
             var postData = postSnapshot.val();
-            console.log("the rides posted are " + postKey);
-            console.log("post and data "+ JSON.stringify (postData));
-            myStateArr.push({postKey : postData});
+            // console.log("the rides posted are " + postKey);
+            // console.log("post and data "+ JSON.stringify (postData));
+            //myStateArr.push({postKey : postData});
             myStateObj[postKey] = postData;
+
+            self.setState({spinnerState: false});
+            console.log("myArrWithData " + JSON.stringify(myStateObj));
+            self.setState({
+              dataSource : myStateObj,
+            });
+
+
           });
+
         });
       });
     }
-    console.log("myArrWithData " + JSON.stringify(myStateObj));
-    this.setState({
-      dataSource : myStateObj
-    })
   }
-
 
   componentDidMount(){
     this.getTestData();
     this.timer = setInterval( () => {
       this.getTestData();
-    }, 3000)
+    }, 4500)
   }
 
   componentWillUnmount() {
@@ -106,11 +106,13 @@ export default class MyPosts extends Component {
       </Header>
 
       <Content>
-      <Spinner color='green' />
+      {renderIf(this.state.spinnerState)(
+        <Spinner color='#e74c3c' />
+      )}
         <List dataArray={this.state.dataSource}
             renderRow={(item) =>
               <ListItem button onPress={() => {this._navigateEditPost(this, item)}}>
-              <Text> My price is {item.price}, for a {item.type}. {"\n"}It is at {item.address} </Text>
+              <Text>My price is {item.price}, for a {item.type}. {"\n"}It is at {item.address} </Text>
               </ListItem>
               }>
         </List>
@@ -130,7 +132,6 @@ const styles = StyleSheet.create({
   welcome: {
     fontSize: 50,
     textAlign: 'center',
-    margin: 10,
     marginTop: 100,
   },
 });
