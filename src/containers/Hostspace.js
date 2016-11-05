8876175c8472b60717c0f195442a945915c8ae05
Matthew Kim <madkim@ucsc.edu>
2016-11-04 18:48:59 -0700

    Added picker for states instead of text field. Also made submit button into a block button.

--- src/containers/Hostspace.js
+++ src/containers/Hostspace.js
@@ -9,6 +9,7 @@ export default class Hostspace extends Component {
     super(props);
     this.state = {text : 'this text will be updated by typing',
     type : 'Garage',
+    us_state : 'Alabama',
     price : 1,
     hideButton: false
   };
@@ -25,7 +26,7 @@ enableButton(){
   let myAddress=this.state.address;
   let myType=this.state.type;
   let myCity = this.state.city;
-  let myState = this.state.state;
+  let myState = this.state.us_state;
   //let mLat = this.state.lat;
   //let mLong = this.state.long;
   let myUsername = "";
@@ -47,7 +48,7 @@ enableButton(){
     let myAddress=this.state.address;
     let myType=this.state.type;
     let myCity = this.state.city;
-    let myState = this.state.state;
+    let myState = this.state.us_state;
     //let mLat = this.state.lat;
     //let mLong = this.state.long;
     let myUsername = "";
@@ -67,7 +68,7 @@ enableButton(){
         price: myPrice,
         address: myAddress,
         city: myCity,
-        state: myState,
+        us_state: myState,
         type : myType,
         poster: myUsername,
         available : true
@@ -203,16 +204,64 @@ enableButton(){
                 }}
                   value = {this.state.city}/>
 
-                <TextInput style={styles.options}
-                  placeholder = "State"
-                  onChangeText={(state) =>
-                    {this.setState({state: state})
-                  }}
-                    value = {this.state.state}/>
+                <Text style={styles.options}>State:</Text>
+                <Picker style={styles.picker}
+                  selectedValue={this.state.us_state}
+                  onValueChange={(us_state) => this.setState({us_state: us_state})}>
+                  <Picker.Item label="Alabama"        value="Alabama" />
+                  <Picker.Item label="Alaska"         value="Alaska" />
+                  <Picker.Item label="Arizona"        value="Arizona" />
+                  <Picker.Item label="Arkansas"       value="Arkansas" />
+                  <Picker.Item label="California"     value="California" />
+                  <Picker.Item label="Colorado"       value="Colorado" />
+                  <Picker.Item label="Connecticut"    value="Connecticut" />
+                  <Picker.Item label="Delaware"       value="Delaware" />
+                  <Picker.Item label="Florida"        value="Florida" />
+                  <Picker.Item label="Georgia "       value="Georgia " />
+                  <Picker.Item label="Hawaii"         value="Hawaii" />
+                  <Picker.Item label="Idaho"          value="Idaho" />
+                  <Picker.Item label="Illinois"       value="Illinois" />
+                  <Picker.Item label="Indiana"        value="Indiana" />
+                  <Picker.Item label="Iowa"           value="Iowa" />
+                  <Picker.Item label="Kansas"         value="Kansas" />
+                  <Picker.Item label="Kentucky"       value="Kentucky" />
+                  <Picker.Item label="Louisiana"      value="Louisiana" />
+                  <Picker.Item label="Maine"          value="Maine" />
+                  <Picker.Item label="Maryland"       value="Maryland" />
+                  <Picker.Item label="Massachusetts"  value="Massachusetts" />
+                  <Picker.Item label="Michigan"       value="Michigan" />
+                  <Picker.Item label="Minnesota "     value="Minnesota " />
+                  <Picker.Item label="Mississippi "   value="Mississippi " />
+                  <Picker.Item label="Missouri"       value="Missouri" />
+                  <Picker.Item label="Montana"        value="Montana" />
+                  <Picker.Item label="Nebraska"       value="Nebraska" />
+                  <Picker.Item label="Nevada"         value="Nevada" />
+                  <Picker.Item label="New Hampshire"  value="New Hampshire" />
+                  <Picker.Item label="New Jersey"     value="New Jersey" />
+                  <Picker.Item label="New Mexico"     value="New Mexico" />
+                  <Picker.Item label="New York"       value="New York" />
+                  <Picker.Item label="North Carolina" value="North Carolina" />
+                  <Picker.Item label="North Dakota"   value="North Dakota" />
+                  <Picker.Item label="Ohio"           value="Ohio" />
+                  <Picker.Item label="Oklahoma"       value="Oklahoma" />
+                  <Picker.Item label="Oregon"         value="Oregon" />
+                  <Picker.Item label="Pennsylvania"   value="Pennsylvania" />
+                  <Picker.Item label="Rhode Island"   value="Rhode Island" />
+                  <Picker.Item label="South Carolina" value="South Carolina" />
+                  <Picker.Item label="South Dakota"   value="South Dakota" />
+                  <Picker.Item label="Tennessee"      value="Tennessee" />
+                  <Picker.Item label="Texas"          value="Texas" />
+                  <Picker.Item label="Utah"           value="Utah" />
+                  <Picker.Item label="Vermont"        value="Vermont" />
+                  <Picker.Item label="Virginia"       value="Virginia" />
+                  <Picker.Item label="Washington"     value="Washington" />
+                  <Picker.Item label="West Virginia"  value="West Virginia" />
+                  <Picker.Item label="Wisconsin"      value="Wisconsin" />
+                  <Picker.Item label="Wyoming"        value="Wyoming" />
+                </Picker>
 
-                <Button  style={styles.options}
+                <Button large block 
                   onPress={() => { this._handlePress(); }}>
-
                   Submit New Space
                 </Button>
 
